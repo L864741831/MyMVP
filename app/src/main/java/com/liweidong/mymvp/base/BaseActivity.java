c18be69210a3739f6fb02e4392fe164356ab1f48
Tony <864741831@qq.com>
2018-10-12 17:05:35 +0800

    MVP + RxJava2 + Retrofit2 + Dagger2  + Room + Glide 开发框架

--- app/src/main/java/com/liweidong/mymvp/base/BaseActivity.java
+++ app/src/main/java/com/liweidong/mymvp/base/BaseActivity.java
@@ -37,6 +37,7 @@ public abstract class BaseActivity extends SupportActivity implements BaseView, 
 
     private final String TAG = this.getClass().getName();
 
+    //创建视图绑定view
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -78,6 +79,7 @@ public abstract class BaseActivity extends SupportActivity implements BaseView, 
     protected void initEvent() {
     }
 
+    //启动使试图可见，该方法快速
     @Override
     protected void onStart() {
         super.onStart();
@@ -90,6 +92,7 @@ public abstract class BaseActivity extends SupportActivity implements BaseView, 
         Log.i(TAG, this.getClass().getName() + "------>onRestart");
     }
 
+    //用户获取焦点，大多数初始化和操作
     //onPause后重新获取数据或初次获取数据
     @Override
     protected void onResume() {
@@ -97,18 +100,24 @@ public abstract class BaseActivity extends SupportActivity implements BaseView, 
         Log.i(TAG, this.getClass().getName() + "------>onResume");
     }
 
+    //失去焦点,但可能部分可见，例如多窗口模式
     @Override
     protected void onPause() {
         super.onPause();
         Log.i(TAG, this.getClass().getName() + "------>onPause");
     }
 
+    //完全不可见,在运行时没机会保存数据库可以在这里保存
     @Override
     protected void onStop() {
         super.onStop();
         Log.i(TAG, this.getClass().getName() + "------>onStop");
     }
 
+    //销毁当前界面,但进程中可能保留activity和视图对象
+    //当界面finash或返回键销毁，则完全销毁
+    //当因为界面发生变化，如屏幕旋转，则会保留成员变量或View中数据，如EditText中已经输入内容，保存在Bundle savedInstanceState
+    //view需要设置id才能保留view中数据
     @Override
     protected void onDestroy() {
         super.onDestroy();
